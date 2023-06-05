f27d74e6b9c348738473f433e1f881563763f97d
KarolinaCetera <karolina.cetera@gmail.com>
2023-06-05 20:32:16 +0200

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
